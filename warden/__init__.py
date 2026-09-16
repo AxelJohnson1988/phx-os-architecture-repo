50ec25a3e1ac540ba984722949415f15d19d5df2
Jakob Axel Paper <JAKOBPAPER@GMAIL.COM>
2026-09-16 15:26:00 -0500

    Add Warden reference package

---
+++ warden/__init__.py
@@ -0,0 +1,5 @@
+"""Minimal reference implementation of the Phoenix Warden boundary."""
+
+from .kernel import WardenKernel, WardenReceipt, WardenRequest
+
+__all__ = ["WardenKernel", "WardenReceipt", "WardenRequest"]
